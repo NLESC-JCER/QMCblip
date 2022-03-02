46bfab12dcc5b478b3084bcc8aa63850b54d0a2c
Emiel Slootman <emiel.slootman.201@gmail.com>
2022-03-02 13:54:28 +0100

    Added ASE calculator for CHAMP

---
+++ champ.py
@@ -0,0 +1,73 @@
+from ase.calculators.calculator import FileIOCalculator, CalculatorSetupError
+from ase.calculators.calculator import InputError
+import numpy as np
+from ase.units import Ang, Bohr, Ha
+from os import path
+
+class CHAMP(FileIOCalculator):
+
+    implemented_properties = ['energy', 'forces']
+
+    name = "CHAMP"
+
+    default_parameters = dict(
+        vmc_in='vmc.inp',
+        vmc_out='vmc.out',
+        force_file='champ_forces',
+        pos_file='molecule.xyz',
+        champ_loc='/usr/bin/vmc.mov1',
+        ncore=1)
+
+    command = ""
+
+    def __init__(self, restart=None,
+                ignore_bad_restart_file=FileIOCalculator._deprecated,
+                label='CHAMP', atoms=None, **kwargs):
+        
+        
+        FileIOCalculator.__init__(self, restart, ignore_bad_restart_file,
+                                label, atoms, **kwargs)
+
+        if (not path.exists(self.parameters['champ_loc'])):
+            raise CalculatorSetupError("Did not find champ at: " + self.parameters['champ_loc'])
+            
+        if (not path.exists(self.parameters['vmc_in'])):
+            raise CalculatorSetupError("Problem reading CHAMP input.")
+
+        self.command = "mpirun -n " + str(self.parameters['ncore']) + " " \
+                    + self.parameters['champ_loc'] + " -i " + self.parameters['vmc_in'] \
+                    + " > " + self.parameters['vmc_out']
+
+
+    def write_input(self, atoms, properties=None, system_changes=None):
+        FileIOCalculator.write_input(self, atoms, properties, system_changes)
+
+        with open(self.parameters['pos_file'], 'w') as fileobj:
+            comment = atoms.symbols
+            natoms = len(atoms)
+            fmt='%22.15f'
+
+            fileobj.write('%d\n%s\n' % (natoms, comment))
+
+            for s, (x, y, z) in zip(atoms.symbols, atoms.positions):
+                fileobj.write('%-2s %s %s %s\n' % (s, fmt % (x * Ang/Bohr), \
+                                fmt % (y * Ang/Bohr), fmt % (z * Ang/Bohr)))
+
+    def read(self, label):
+        raise NotImplementedError
+
+    def read_results(self):
+        if (not path.exists(self.parameters['force_file'])):
+            raise InputError("Could not detect the forces from CHAMP in " \
+                                        + self.parameters['force_file'])
+
+        with open(self.parameters['force_file'], 'r') as fileobj:
+            self.results['energy'] = float(fileobj.readline()) * Ha
+
+            forces = []
+            for line in fileobj.readlines():
+                f_list = [float(i) for i in line.split(" ") if i.strip()]
+                forces.append(f_list)
+
+            self.results['forces'] = -np.asarray(forces) * Ha/Bohr    
+            print(self.results['forces'])   
