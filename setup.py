c1c949b12047ef689ef4ef864dd3cd0f8331e50c
Emiel Slootman <emiel.slootman.201@gmail.com>
2022-04-14 10:53:30 +0200

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="QMCblip",
-    version="1.0.2",
+    version="1.0.3",
     author="Emiel Slootman",
     author_email="e.slootman@esciencecenter.nl",
     description="A small package to couple Quantum Monte Carlo codes to Machine Learning Force Fields through ASE.",
