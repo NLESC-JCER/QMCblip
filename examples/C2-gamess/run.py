a61f732f02f7d69bdacd7203bdaa762a13906815
Emiel Slootman <emiel.slootman.201@gmail.com>
2022-04-26 13:18:59 +0200

    Changes GAMESS to presets

--- examples/C2-gamess/run.py
+++ examples/C2-gamess/run.py
@@ -1,16 +1,9 @@
-from ase import Atoms
-from qmcblip.gamess.utils import WavefunctionCreator
+from qmcblip.gamess.utils import Presets
 from qmcblip.champ import CHAMP
-from qmcblip.champio import Settings
 
-atoms = Atoms('C2', [(0,0,-0.7), (0,0, 0.7)])
-wf = WavefunctionCreator(atoms, '../../champ')
+atoms = Presets.C2.atoms
 
-wf.setup_rhf()
-wf.setup_cas(system=dict(mwords=500), drt=dict(nmcc=2, ndoc=2, nval=2))
-wf.setup_ci(system=dict(mwords=500), cidrt=dict(nfzc=2, ndoc=2, nval=2))
-wf.convert_to_champ()
-input = wf.create_champ_input()
+input = Presets.C2().build('../../champ')
 
 input.optwf.nopt_iter = 100
 
--- tests/gamess/test_gamess.py
+++ tests/gamess/test_gamess.py
@@ -6,8 +6,7 @@ from pathlib import Path
 import pytest
 from ase import Atoms
 from qmcblip.champ import CHAMP
-from qmcblip.champio import Settings
-from qmcblip.gamess.utils import WavefunctionCreator
+from qmcblip.gamess.utils import Presets
 
 found_champ = pytest.mark.skipif(
     not Path.home().joinpath(Path('software/champ')).is_dir(), reason="CHAMP not found."
@@ -25,28 +24,11 @@ class TestGamess(unittest.TestCase):
         os.mkdir("tests/test_data/temp")
         os.chdir('tests/test_data/temp')
 
-    @found_gamess
-    @found_champ
-    def test_userscrError(self):
-        atoms = Atoms('C2', [(0,0,-0.61385), (0,0,0.61385)])
-        wf = WavefunctionCreator(atoms, str(Path.home().joinpath('software/champ')))
-        with self.assertRaises(RuntimeError):
-            wf.setup_rhf(userscr=None)
-        os.chdir('..')
-        with self.assertRaises(FileNotFoundError):
-            wf.setup_rhf(userscr="not_exist")
-        os.chdir('..')
-
-
     @found_champ
     @found_gamess
     def test_gamess(self):
-        atoms = Atoms('C2', [(0,0,-0.61385), (0,0,0.61385)])
-        wf = WavefunctionCreator(atoms, str(Path.home().joinpath('software/champ')))
-        wf.setup_rhf(userscr=str(Path.home().joinpath(Path('software/gamess/restart'))))
-        wf.setup_cas(system=dict(mwords=500), drt=dict(nmcc=2, ndoc=2, nval=2), userscr=str(Path.home().joinpath(Path('software/gamess/restart'))))
-        wf.setup_ci(system=dict(mwords=500), cidrt=dict(nfzc=2, ndoc=2, nval=2), userscr=str(Path.home().joinpath(Path('software/gamess/restart'))))
-        wf.convert_to_champ()
+        atoms = Presets.C2.atoms
+        input = Presets.C2(userscr=str(Path.home().joinpath(Path('software/gamess/restart')))).build(str(Path.home().joinpath('software/champ')))
         input = wf.create_champ_input()
         input.optwf.nopt_iter = 10
         input.write('vmc.inp')
